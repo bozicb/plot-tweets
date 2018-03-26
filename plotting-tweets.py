739d935773d089ed34fa04ac8e23aadaa48deac6
Bojan Božić <boj.bozic@gmail.com>
2018-03-26 10:20:00 +0100

    user age plot added

--- plotting-tweets.py
+++ plotting-tweets.py
@@ -1,9 +1,13 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+from datetime import datetime
 
 tweets=pd.read_csv("tweets.csv")
-print(tweets.head())
+tweets['created']=pd.to_datetime(tweets['created'])
+tweets['user_created']=pd.to_datetime(tweets['user_create'])
+tweets['user_age']=tweets['user_created'].apply(
+    lambda x: (datetime.now()-x).total_seconds()/3600/24/365)
 
 def get_candidate(row):
     candidates=[]
@@ -20,5 +24,7 @@ tweets['candidate']=tweets.apply(get_candidate,axis=1)
 counts=tweets['candidate'].value_counts()
 plt.bar(range(len(counts)),counts)
 plt.show()
-
 print(counts)
+
+plt.hist(tweets['user_age'])
+plt.show
