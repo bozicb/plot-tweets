4dcdf6cb650003945bdf0b169df85102f1e33962
Bojan Božić <boj.bozic@gmail.com>
2018-03-26 13:49:09 +0100

    stacking and annotation

--- plotting-tweets.py
+++ plotting-tweets.py
@@ -5,9 +5,9 @@ from datetime import datetime
 
 tweets=pd.read_csv("tweets.csv")
 tweets['created']=pd.to_datetime(tweets['created'])
-tweets['user_created']=pd.to_datetime(tweets['user_create'])
+tweets['user_created']=pd.to_datetime(tweets['user_created'])
 tweets['user_age']=tweets['user_created'].apply(
-    lambda x: (datetime.now()-x).total_seconds()/3600/24/365)
+    lambda x: ((datetime.now()-x).total_seconds()/3600/24/365)-2)
 
 def get_candidate(row):
     candidates=[]
@@ -22,9 +22,17 @@ def get_candidate(row):
 
 tweets['candidate']=tweets.apply(get_candidate,axis=1)
 counts=tweets['candidate'].value_counts()
-plt.bar(range(len(counts)),counts)
-plt.show()
-print(counts)
 
-plt.hist(tweets['user_age'])
-plt.show
+cl_tweets=tweets['user_age'][tweets['candidate']=='clinton']
+sa_tweets=tweets['user_age'][tweets['candidate']=='sanders']
+tr_tweets=tweets['user_age'][tweets['candidate']=='trump']
+
+plt.hist([cl_tweets,sa_tweets,tr_tweets],stacked=True,
+         label=['clinton','sanders','trum'])
+plt.legend()
+plt.title('Tweets mentioning each candidate')
+plt.xlabel('Twitter account age in years')
+plt.ylabel('# of tweets')
+plt.annotate('More Trump tweets',xy=(1,35000),xytext=(2,35000),
+             arrowprops=dict(facecolor='black'))
+plt.show()
